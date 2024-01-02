bb8232e846f4983a803dbe7a79f29612833c10d0
noonever <noonevernowhere@gmail.com>
2024-01-02 17:43:36 +0300

    bad deps

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,12 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ignoredRouteFiles: ["**/.*"],
+  serverDependenciesToBundle: [
+    /remix-utils/ 
+  ],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
   // serverBuildPath: "build/index.js",
 };
+
